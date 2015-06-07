f4fe49565a8a8ddde5bf322c924d01ee77eecd22
mrpatrickwest <westp@rpi.edu>
2015-06-07 14:15:45 -0400

    using /browsers as the root of the dco s2s browsers

--- js/s2s-config.js
+++ js/s2s-config.js
@@ -6,11 +6,11 @@
     s2s.utils.metadataService = s2s.utils.servletRoot + "metadata";
     s2s.utils.proxyService = s2s.utils.servletRoot + "proxy";
     s2s.utils.sessionService = "http://data.deepcarbon.net:8081/s2s/session";
-    s2s.utils.searchWidgetClass = "http://data.deepcarbon.net/dco-s2s/ontologies/s2s/4/0/InputWidget";
-    s2s.utils.resultsWidgetClass = "http://data.deepcarbon.net/dco-s2s/ontologies/s2s/4/0/ResultsWidget";
-    s2s.utils.resultsQueryUri = "http://data.deepcarbon.net/dco-s2s/ontologies/s2s/4/0/SearchResultsInterface";
-    s2s.utils.hierarchicalSearch = "http://data.deepcarbon.net/dco-s2s/ontologies/s2s-core/4/0/HierarchicalSearch";
-    s2s.utils.facetedSearch = "http://data.deepcarbon.net/dco-s2s/ontologies/s2s-core/4/0/FacetedSearch";
+    s2s.utils.searchWidgetClass = "http://data.deepcarbon.net/browsers/ontologies/s2s/4/0/InputWidget";
+    s2s.utils.resultsWidgetClass = "http://data.deepcarbon.net/browsers/ontologies/s2s/4/0/ResultsWidget";
+    s2s.utils.resultsQueryUri = "http://data.deepcarbon.net/browsers/ontologies/s2s/4/0/SearchResultsInterface";
+    s2s.utils.hierarchicalSearch = "http://data.deepcarbon.net/browsers/ontologies/s2s-core/4/0/HierarchicalSearch";
+    s2s.utils.facetedSearch = "http://data.deepcarbon.net/browsers/ontologies/s2s-core/4/0/FacetedSearch";
 
     /**
      * Built-in behavior for list-style results
