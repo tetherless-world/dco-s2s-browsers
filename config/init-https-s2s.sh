307d40e8e00a046e7b28e322da4f0a5709a26407
Stephan Zednik <lokipatera@gmail.com>
2015-06-18 08:35:32 -0600

    Update init-https-s2s.sh

--- config/init-https-s2s.sh
+++ config/init-https-s2s.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# The S2S metadata service returns information about the specified search service
+# At present the S2S service does not appear to be able to resolve RDF over HTTPS 
+# so the S2S config RDF will have to be loaded over HTTP before the metadata service
+# can work with search service instances that use HTTPS
+
+# This script should be run after redeploying the S2S servlet to load the S2S config RDF over HTTP
+
+# author: szednik (zednis2@rpi.edu)
+
 AVAILABLE_DEPLOYMENTS=" deepcarbon.net
  deepcarbon.net-staging
  dcotest.tw.rpi.edu"
@@ -11,17 +20,26 @@ case $1 in
     case ${DEPLOYMENT} in
 
         "deepcarbon.net")
-        `curl -L 'https://data.deepcarbon.net:8444/s2s/metadata?type=services&instance=http%3A//data.deepcarbon.net/browsers/ontologies/dco-s2s.ttl%23PersonSearchService'`
+        result = `curl -L 'https://data.deepcarbon.net:8444/s2s/metadata?type=services&instance=http%3A//data.deepcarbon.net/browsers/ontologies/dco-s2s.ttl%23PersonSearchService'`
+        if [ "$result" -ne '{}' ]; then
+          echo "S2S metadata service did not return the expected response"
+        fi
         echo "configured HTTPS s2s for deployment to ${DEPLOYMENT}"
         ;;
         
         "deepcarbon.net-staging")
-        `curl -L 'https://data.deepcarbon.net:8444/s2s/metadata?type=services&instance=http%3A//data.deepcarbon.net/staging/browsers/ontologies/dco-s2s.ttl%23PersonSearchService'`
+        result = `curl -L 'https://data.deepcarbon.net:8444/s2s/metadata?type=services&instance=http%3A//data.deepcarbon.net/staging/browsers/ontologies/dco-s2s.ttl%23PersonSearchService'`
+        if [ "$result" -ne '{}' ]; then
+          echo "S2S metadata service did not return the expected response"
+        fi
         echo "configured HTTPS s2s for deployment to ${DEPLOYMENT}"
         ;;
 
         "dcotest.tw.rpi.edu")
-        `curl -L 'https://dcotest.tw.rpi.edu/s2s/metadata?type=services&instance=http%3A//dcotest.tw.rpi.edu/browsers/ontologies/dco-s2s.ttl%23PersonSearchService'`
+        result = `curl -L 'https://dcotest.tw.rpi.edu/s2s/metadata?type=services&instance=http%3A//dcotest.tw.rpi.edu/browsers/ontologies/dco-s2s.ttl%23PersonSearchService'`
+        if [ "$result" -ne '{}' ]; then
+          echo "S2S metadata service did not return the expected response"
+        fi
         echo "configured HTTPS s2s for deployment to ${DEPLOYMENT}"
         ;;
 
